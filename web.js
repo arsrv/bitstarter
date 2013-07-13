c67c16fc7abe482456e7a7c4b8c468f5b609f4f1
Ubuntu <ubuntu@ip-10-116-37-158.ec2.internal>
2013-07-13 16:40:05 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile("./index.html", function (err,data) {
+  fs.readFile("index.html", function (err,data) {
     if (err) {
       response.send("Houston, we have a problem.");
       console.log("Failed to read index.htmlo");
