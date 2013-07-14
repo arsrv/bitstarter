0f7275619dae37bad7c251af0385cb32d8ac55f6
Ubuntu <ubuntu@ip-10-116-37-158.ec2.internal>
2013-07-14 14:13:03 +0000

    grader.js updated

--- grader.js
+++ grader.js
@@ -26,6 +26,9 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var GITHUBPATH_DEFAULT = "http://limitless-cove-6039.herokuapp.com";
+//var sys = require("util");
+var rest = require("restler"); // https://github.com/danwrong/restler
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,14 +64,37 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkHtmlFileAsync = function(url, checksfile) {
+  rest.get(url.toString()).on("complete", function(result) {
+    if (result instanceof Error) {
+      console.log("%s does not exist. Existing.", url.toString());
+      process.exit(1);
+    }
+    $ = cheerio.load(result.toString());
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
+  });
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_address>', 'HTTP Path to the index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+      checkHtmlFileAsync(program.url, program.checks);
+    } else if (program.file) {
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
